9e2de5052b21ccd309b2511a920e5e6b60c39319
mlafefon <51256464+mlafefon@users.noreply.github.com>
2026-02-27 23:20:03 +0200

    Update PWA scope and start URL in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -26,8 +26,8 @@ export default defineConfig(({mode}) => {
           background_color: '#ffffff',
           display: 'standalone',
           orientation: 'portrait',
-          scope: base,
-          start_url: base,
+          scope: '/split-trip/',
+          start_url: '/split-trip/',
           icons: [
             {
               src: 'pwa-icon.svg',
